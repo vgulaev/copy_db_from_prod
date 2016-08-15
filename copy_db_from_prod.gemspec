7c9b362fef2ec3e7a9d8cd301b182fb0ee475d17
Valentin <vgulaev@yandex.ru>
2016-08-15 15:12:49 +0300

    Second commit:))

--- copy_db_from_prod.gemspec
+++ copy_db_from_prod.gemspec
@@ -1,14 +1,15 @@
-Gem::Specification.new do |gem|
-  gem.name        = 'copy_db_from_prod'
-  gem.version     = '0.0.0'
-  gem.date        = '2016-08-15'
-  gem.summary     = "Hola!"
-  gem.description = "A simple hello world gem"
-  gem.authors     = ["vgulaev"]
-  gem.email       = 'vgulaev@yandex.ru'
-  gem.files       = ["lib/copy_db_from_prod.rb"]
-  gem.executables   = ["copy_db_from_prod"]
-  gem.homepage    =
-    'http://rubygems.org/gems/copy_db_from_prod'
-  gem.license       = 'MIT'
+Gem::Specification.new do |spec|
+  spec.name        = 'copy_db_from_prod'
+  spec.version     = '0.0.0'
+  spec.date        = '2016-08-15'
+  spec.summary     = "Hola!"
+  spec.description = "A simple hello world gem"
+  spec.authors     = ["vgulaev"]
+  spec.email       = 'vgulaev@yandex.ru'
+  spec.files       = ["lib/copy_db_from_prod.rb"]
+  spec.executables = ["copy_db_from_prod"]
+  spec.homepage    = 'http://rubygems.org/gems/copy_db_from_prod'
+  spec.license     = 'MIT'
+
+  spec.add_runtime_dependency('net-ssh')
 end
--- lib/copy_db_from_prod.rb
+++ lib/copy_db_from_prod.rb
@@ -1,5 +1,36 @@
+require 'net/ssh'
+require 'yaml'
+
 class CopyDbFromProd
+  attr_accessor :cf
+
   def self.hi
     puts "Hello world!"
+  end
+
+  def load_conf(args)
+    @cf = { host: args.host, user: args.user,
+            keys: [File.open("#{ENV['HOME']}/.ssh/id_rsa", 'r').read],
+            deploy_to: '/projects/insales3' }
+  end
+
+  def initialize(args)
+    load_conf(args)
+  end
+
+  def download_schema
+    Net::SSH.start(cf[:host], cf[:user], key_data: cf[:keys], keys_only: TRUE) do |ssh|
+      @prod_conf = YAML.load(ssh.exec!("cat #{cf[:deploy_to]}/config/database.yml"))['production']
+      return Zlib::GzipReader.new(StringIO.new(ssh.exec!(dump_schema))).read
+    end
+  end
+
+  def dump_schema
+    cmd = []
+    { password: 'export PGPASSWORD=#{val} &&', database: 'pg_dump #{val}', host: '-h #{val}', port: '-p #{val}',
+      username: '-U #{val}' }.each_pair do |key, val|
+      cmd.push(val.sub('#{val}', @prod_conf[key.to_s].to_s)) if @prod_conf.key?(key.to_s)
+    end
+    "#{cmd.join(' ')} --schema-only --no-owner --no-privileges -Z9"
   end
 end

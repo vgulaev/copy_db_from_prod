5b4f13bd0e48f57af90f74b075305f66fb0d0683
Valentin <vgulaev@yandex.ru>
2016-08-16 15:07:30 +0300

    Work release

--- lib/copy_db_from_prod.rb
+++ lib/copy_db_from_prod.rb
@@ -3,7 +3,7 @@ require 'yaml'
 require 'pg'
 
 class CopyDbFromProd
-  attr_accessor :cf
+  attr_accessor :cf, :conn
 
   def self.hi
     puts "Hello world!"
@@ -21,12 +21,21 @@ class CopyDbFromProd
 
   def download_schema
     Net::SSH.start(cf[:host], cf[:user], key_data: cf[:keys], keys_only: TRUE) do |ssh|
-      @prod_conf = YAML.load(ssh.exec!("cat #{cf[:deploy_to]}/config/database.yml"))['production']
+      yml_text = ssh.exec!("cat #{cf[:deploy_to]}/config/database.yml")
+      begin
+        @prod_conf ||= YAML.load(yml_text)['production']
+      rescue => e
+        raise "Can't access to database.yml, check -p params\n #{e}"
+      end
       prod_data = ssh.exec!(dump_schema)
       if prod_data.include?('pg_dump') || prod_data.include?('warn')
-        raise "error: pg_dump dosen't work correct \n #{prod_data}"
+        raise "error: pg_dump dosen't work correct\n #{prod_data}"
       else
-        return Zlib::GzipReader.new(StringIO.new(ssh.exec!(prod_data))).read
+        begin
+          return Zlib::GzipReader.new(StringIO.new(prod_data)).read
+        rescue => e
+          raise "#{e}\n #{prod_data}"
+        end
       end
     end
   end
@@ -40,18 +49,39 @@ class CopyDbFromProd
     "#{cmd.join(' ')} --schema-only --no-owner --no-privileges -Z9"
   end
 
+  def drop_restore_table
+    conn.exec("SELECT table_name
+                FROM information_schema.tables
+                WHERE table_schema = 'public'
+                AND table_name LIKE '%_201%'
+                ORDER BY table_name"
+    ) do |result|
+      conn.exec(result.map { |row| "DROP TABLE #{row['table_name']}" }.join(';'))
+    end
+  end
+
   def load_schema
     @dev_conf = YAML.load(File.open('config/database.yml', 'r').read)['development']
-    conn = PGconn.open(host: @dev_conf['host'],
+    @conn = PGconn.open(host: @dev_conf['host'],
                      dbname: 'postgres',
                        user: @dev_conf['username'],
                    password: @dev_conf['password'])
     conn.exec("DROP DATABASE IF EXISTS #{@dev_conf['database']}")
     conn.exec("CREATE DATABASE #{@dev_conf['database']}")
-    conn = PGconn.open(host: @dev_conf['host'],
+    @conn = PGconn.open(host: @dev_conf['host'],
                      dbname: @dev_conf['database'],
                        user: @dev_conf['username'],
                    password: @dev_conf['password'])
-    conn.exec(download_schema)
+    schema = download_schema
+    begin
+      conn.exec(schema)
+    rescue => e
+      raise "Database #{@dev_conf['database']}\n#{e}\n #{schema}"
+    end
+    drop_restore_table if 'insales_dev' == @dev_conf['database']
+  end
+
+  def copy_env_table
+
   end
 end
--- upgem.rb
+++ upgem.rb
@@ -1,2 +1,3 @@
 system("gem build copy_db_from_prod.gemspec")
-system("cd ~/workspaces/ekam_dev/ekam && gem install ~/workspaces/copy_db_from_prod/copy_db_from_prod-0.0.0.gem")
+#system("cd ~/workspaces/ekam_dev/ekam && gem install ~/workspaces/copy_db_from_prod/copy_db_from_prod-0.0.1.gem")
+system("cd ~/workspaces/insales_dev/insales && gem install ~/workspaces/copy_db_from_prod/copy_db_from_prod-0.0.1.gem")
